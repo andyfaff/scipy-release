0219771ed47a52c4e2623c43d6d505f96044ba51
Andrew Nelson <andyfaff@gmail.com>
2026-08-20 19:04:42 +1000

    ENH: pin all dependencies

--- tools/wheels/cibw_before_build.sh
+++ tools/wheels/cibw_before_build.sh
@@ -16,6 +16,8 @@ elif [[ $RUNNER_OS == "Windows" ]]; then
     cat $PROJECT_DIR/tools/wheels/LICENSE_win32.txt >> $SCIPY_SRC_DIR/LICENSE.txt
 fi
 
+
+# further checks to determine if openblas is to be installed
 if [[ $(python -c"import sys; print(sys.maxsize)") < $(python -c"import sys; print(2**33)") ]]; then
     echo "No BLAS used for 32-bit wheels"
     export INSTALL_OPENBLAS=false
@@ -24,18 +26,33 @@ elif [ -z $INSTALL_OPENBLAS ]; then
     export INSTALL_OPENBLAS=true
 fi
 
-# Install OpenBLAS from scipy-openblas32|64
-if [[ "$INSTALL_OPENBLAS" = "true" ]] ; then
-    # By default, use scipy-openblas32
-    # On 32-bit platforms and on win-arm64, use scipy-openblas32
-    OPENBLAS=openblas32
-    # Possible values for RUNNER_ARCH in GitHub Actions are: X86, X64, ARM, or ARM64
-    if [[ $RUNNER_ARCH == "X86" || $RUNNER_ARCH == "ARM" ]] ; then
-        OPENBLAS=openblas32
-    elif [[ $RUNNER_ARCH == "ARM64" && $RUNNER_OS == "Windows" ]] ; then
-        OPENBLAS=openblas32
-    fi
 
+# By default, use scipy-openblas32
+# On 32-bit platforms and on win-arm64, use scipy-openblas32
+OPENBLAS=openblas32
+
+# do we install OpenBLAS as part of the build dependencies?
+if [[ "$INSTALL_OPENBLAS" = "true" ]] ; then
+    OPENBLAS_GRP="--group ${OPENBLAS}"
+else
+    OPENBLAS_GRP=""
+fi
+
+
+# install build dependencies via uv
+# uv.lock file needs to be in the same location as pyproject.toml
+cp uv.lock $SCIPY_SRC_DIR
+pushd $SCIPY_SRC_DIR
+# check that the uv lock file is up-to-date
+uv lock --check
+popd
+PYTHON_EXE="$(python -c 'import sys; print(sys.executable)')"
+uv export --project "$SCIPY_SRC_DIR" --no-default-groups --group build --no-emit-project $OPENBLAS_GRP --frozen | \
+    uv pip install --python "$PYTHON_EXE" --no-deps --require-hashes -r -
+
+
+# Configure the pkg-config file for OpenBLAS
+if [[ "$INSTALL_OPENBLAS" = "true" ]] ; then
     # The PKG_CONFIG_PATH environment variable will be pointed to this path in
     # cibuildwheel.toml and .github/workflows/wheels.yml. Note that
     # `pkgconf_path` here is only a bash variable local to this file.
@@ -43,9 +60,6 @@ if [[ "$INSTALL_OPENBLAS" = "true" ]] ; then
     echo pkgconf_path is $pkgconf_path, OPENBLAS is ${OPENBLAS}
     rm -rf $pkgconf_path
     mkdir -p $pkgconf_path
-    pushd $PROJECT_DIR/scipy-src
-    python -m pip install --group ${OPENBLAS}
-    popd
     python -c "import scipy_${OPENBLAS}; print(scipy_${OPENBLAS}.get_pkg_config())" > $pkgconf_path/scipy-openblas.pc
 
     # Copy scipy-openblas DLL's to a fixed location so we can point delvewheel
@@ -60,8 +74,10 @@ EOF
     fi
 fi
 
-# cibuildwheel doesn't install delvewheel by default
+
 if [[ $RUNNER_OS == "Windows" ]]; then
+    # TODO: include this requirement in a dependency group? At the moment
+    # it's the only unpinned package.
     # pkgconf - carries out the role of pkg-config.
     # Alternative is pkgconfiglite that you have to install with choco
     python -m pip install -r $PROJECT_DIR/scipy-src/requirements/pkgconf.txt
